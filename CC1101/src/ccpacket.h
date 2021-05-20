a7bef023c8f9df08e3747a4baa371232ef75c283
qitas <qitas@qitas.cn>
2021-05-20 10:57:26 +0800

    add

---
+++ CC1101/src/ccpacket.h
@@ -0,0 +1,46 @@
+#ifndef _CCPACKET_H
+#define _CCPACKET_H
+
+/**
+ * Buffer and data lengths
+ */
+#define CCPACKET_BUFFER_LEN        64
+#define CCPACKET_DATA_LEN          CCPACKET_BUFFER_LEN - 3
+
+/**
+ * Class: CCPACKET
+ *
+ * Description:
+ * CC1101 data packet class
+ */
+struct CCPACKET
+{
+  public:
+    /**
+     * Data length
+     */
+    unsigned char length;
+
+    /**
+     * Data buffer
+     */
+    unsigned char data[CCPACKET_DATA_LEN];
+
+    /**
+     * CRC OK flag
+     */
+    bool crc_ok;
+
+    /**
+     * Received Strength Signal Indication
+     */
+    unsigned char rssi;
+
+    /**
+     * Link Quality Index
+     */
+    unsigned char lqi;
+};
+
+#endif
+
